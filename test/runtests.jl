d20e40e12958f162c64efc189de887083fcdaed5
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2016-04-26 19:36:10 +0530

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using StingerWrapper
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+s = Stinger(5, 0, [0 for i=1:6], [i%5 for i=1:5], [2 for i=1:5], Int64[], Int64[], -2)
+@test insert_edge!(s, 0, 1, 4, 2, 2) == 1
+@test remove_edge!(s, 0, 1, 4) == 1
+@test consistency_check(s, 5) == true
