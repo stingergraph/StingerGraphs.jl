a62ee55922f82227a2e1f55647a894e0f755a4ba
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2016-04-26 15:59:42 +0530

    Basic operations added.

--- src/stinger_core.jl
+++ src/stinger_core.jl
@@ -1,18 +1,12 @@
 import Base.Libdl: dlopen, dlsym
+import Base: unsafe_convert
 
-export Stinger
+export Stinger, remove_edge!, insert_edge!, consistency_check
 
 stinger_core_lib = dlopen("/Users/rohitvarkey/Code/gatech/stinger/build/lib/libstinger_core.dylib")
 
 type Stinger
-    s::Ptr{Void}
-
-    # To allow for maintaining multiple references to the same stinger data structure
-    function Stinger(x::Ptr{Void})
-        s = new(x)
-        finalizer(s, stinger_free)
-        s
-    end
+    handle::Ptr{Void}
 
     #Default constructor to create a Stinger data structure
     function Stinger()
@@ -22,9 +16,79 @@ type Stinger
     end
 end
 
-#Intialize a new Stinger data structure with the ccall
-Stinger() = Stinger(ccall(dlsym(stinger_core_lib, "stinger_new"), Ptr{Void}, ()))
+unsafe_convert{T}(::Type{Ptr{T}}, s::Stinger) = s.handle
 
 function stinger_free(x::Stinger)
-    ccall(dlsym(stinger_core_lib, "stinger_free"), Ptr{Void}, (Ptr{Void},), x.s)
+    # To prevent segfaults
+    if x.handle != C_NULL
+        x.handle = ccall(dlsym(stinger_core_lib, "stinger_free"), Ptr{Void}, (Ptr{Void},), x)
+    end
+end
+
+#Equivalent of stinger_set_initial_edges
+function Stinger(
+    nv::UInt,
+    etype::Int64,
+    offsets::Vector{Int64},
+    adj::Vector{Int64},
+    weights::Vector{Int64},
+    timestamps::Vector{Int64},
+    firsttimestamps::Vector{Int64},
+    singletimestamp::Int64
+    )
+    s = Stinger()
+    if isempty(timestamps)
+        timestamps = C_NULL
+    end
+    if isempty(firsttimestamps)
+        firsttimestamps = C_NULL
+    end
+    ccall(
+        dlsym(stinger_core_lib, "stinger_set_initial_edges"),
+        Void,
+        (Ptr{Void}, UInt, Int64, Ptr{Int64}, Ptr{Int64}, Ptr{Int64}, Ptr{Int64}, Ptr{Int64}, Int64),
+        s, nv, etype, offsets, adj, weights, timestamps, firsttimestamps, singletimestamp
+    )
+    s
+end
+
+function remove_edge!(s::Stinger, etype::Int64, from::Int64, to::Int64)
+    status = ccall(
+        dlsym(stinger_core_lib, "stinger_remove_edge"),
+        Int32,
+        (Ptr{Void}, Int64, Int64, Int64),
+        s, etype, from, to
+    )
+    if status == 0
+        error("Edge not found")
+    end
+    if status == -1
+        error("Error while removing the edge")
+    end
+    status
+end
+
+function insert_edge!(
+    s::Stinger, etype::Int64, from::Int64, to::Int64, weight::Int64, timestamp::Int64
+    )
+    status = ccall(
+        dlsym(stinger_core_lib, "stinger_insert_edge"),
+        Int32,
+        (Ptr{Void}, Int64, Int64, Int64, Int64, Int64),
+        s, etype, from, to, weight, timestamp
+    )
+    if status == -1
+        error("Error while adding edge")
+    end
+    status
+end
+
+function consistency_check(s::Stinger, nv::Int64)
+    status = ccall(
+        dlsym(stinger_core_lib, "stinger_consistency_check"),
+        Int32,
+        (Ptr{Void}, Int64),
+        s, nv
+    )
+    status == 0 #true if 0, else false
 end
