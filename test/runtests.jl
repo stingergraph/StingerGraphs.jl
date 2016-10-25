ca0b52a50c5711e15e2230be6ac489c5333c0dfd
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2016-10-25 02:10:57 -0400

    Modify tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,15 @@ using StingerWrapper
 using Base.Test
 
 s = Stinger(5, 0, [0 for i=1:6], [i%5 for i=1:5], [2 for i=1:5], Int64[], Int64[], -2)
-@test insert_edge!(s, 0, 1, 4, 2, 2) == 1
+@test insert_edge!(s, 0, 1, 5, 2, 2) == 1
 @test outdegree(s, 1) == 1
-@test getsuccessors(s, 1) == [4]
-@test insert_edge!(s, 0, 4, 2, 2, 2) == 1
-@test bfs(s, 1) == [2,4]
-@test remove_edge!(s, 0, 1, 4) == 1
+@test getsuccessors(s, 1) == [5]
+@test insert_edge!(s, 0, 5, 2, 2, 2) == 1
+@test insert_edge!(s, 0, 2, 0, 2, 2) == 1
+@test insert_edge!(s, 0, 3, 4, 2, 2) == 1
+@test bfs(s, 1) == [2, -1, 5, -1, -1,  1]
+@test remove_edge!(s, 0, 1, 5) == 1
 @test outdegree(s, 1) == 0
-@test consistency_check(s, 5) == true
+@test consistency_check(s, 100) == true
 s[max_netypes] = 6
 @test s[max_netypes] == 6
