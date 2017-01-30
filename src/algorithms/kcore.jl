9c5b0ec5a6201175c17c1e81335ea9d3f21c1965
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-01-30 14:20:46 -0500

    API Levels for kcore

--- src/algorithms/kcore.jl
+++ src/algorithms/kcore.jl
@@ -1,11 +1,8 @@
 export kcore
 
-"Find the kcore of the graph."
-function kcore(s::Stinger, nv::Int64)
-
-    labels = zeros(Int64, nv)
-    counts = zeros(Int64, nv)
+function kcore!(s::Stinger, labels::Array{Int64}, counts::Array{Int64})
     kout = Int64(0)
+    assert(length(labels)==length(counts))
     ccall(
         dlsym(stinger_alg_lib, "kcore_find"),
         Void,
@@ -19,8 +16,26 @@ function kcore(s::Stinger, nv::Int64)
         s,
         labels,
         counts,
-        nv,
+        length(counts),
         kout
     )
-    return labels
+end
+
+
+"Find the kcore of the graph."
+function kcore(s::Stinger, nv::Int64)
+    labels = zeros(Int64, nv)
+    counts = zeros(Int64, nv)
+    kcore!(s, labels, counts)
+    return labels, counts
+end
+
+"""
+This version is slower as it makes a call to `stinger_max_active_vertex`,
+which is sequential and runs through every vertex in the graph. If you know
+the maximum number of active vertices, call `kcore(s, nv)` which is faster.
+"""
+function kcore(s::Stinger)
+    nv = get_nv(s)
+    kcore(s, nv)
 end
