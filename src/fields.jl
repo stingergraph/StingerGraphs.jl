4d18c5a7dae5ceedac04fed4afa62eba590280b5
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-02-09 15:54:28 -0500

    All the right code in all the right places.

--- src/fields.jl
+++ src/fields.jl
@@ -1,5 +1,5 @@
 import Base: getindex, setindex!
-export get_nv, storageptr
+export get_nv, getvertex, edgeparse, StingerEdge, StingerVertex
 
 function getindex(x::Stinger, field::StingerFields)
     idx = Int(field)
@@ -43,3 +43,49 @@ end
 
 """Get a pointer to the storage array of Stinger"""
 storageptr(s::Stinger) = s.handle + sizeof(StingerGraph) + 5*sizeof(UInt64)
+
+type StingerVertex
+    vtype::Int64
+    weight::Int64
+    indegree::Int64
+    outdegree::Int64
+    degree::Int64
+    edges::Int64
+end
+
+const NUMEDGEBLOCKS = 14
+
+type StingerEdge
+    neighbor::Int64
+    weight::Int64
+    timefirst::Int64
+    timerecent::Int64
+end
+
+type StingerEdgeBlock
+    next::UInt64
+    etype::Int64
+    vertexid::Int64
+    numedges::Int64
+    high::Int64
+    smallstamp::Int64
+    largestamp::Int64
+    cache_pad::Int64
+end
+
+function edgeparse(edge::StingerEdge)
+    direction = edge.neighbor >> 61 #The first 2 bits denote the direction, 1 - in, 2 - out, 3 - both
+    neighbor = ~(7 << 61) & edge.neighbor
+    return direction, neighbor
+end
+
+function getvertex(s::Stinger, v::Int64)
+    vertices = convert(Ptr{StingerVertex}, storageptr(s) + sizeof(Int64)) #Read the StingerVertex array
+    vertex = unsafe_load(vertices, v+1)
+    vertex
+end
+
+function getvertexedgesoffset(s::Stinger, v::Int64)
+    vertex = stinger_vertex_get(s,v)
+    vertex.edges
+end
---
+++ src/traversal.jl
@@ -0,0 +1,18 @@
+export foralledges
+
+function foralledges(f::Function, s::Stinger, v::Int64)
+    storage = storageptr(s)
+    ebpool = storage + s[ebpool_start] * (sizeof(UInt8))
+    ebpool_priv_ptr = ebpool + sizeof(UInt64) * 2 #gcc seems to pad this
+    current_eb_ptr = ebpool_priv_ptr + getvertex(s, v).edges * (sizeof(StingerEdgeBlock) + sizeof(StingerEdge)*NUMEDGEBLOCKS)
+    while current_eb_ptr != ebpool_priv_ptr
+        current_eb = unsafe_load(convert(Ptr{StingerEdgeBlock}, current_eb_ptr))
+        src = current_eb.vertexid
+        etype = current_eb.etype
+        for i=1:current_eb.high
+            current_edge = unsafe_load(convert(Ptr{StingerEdge}, current_eb_ptr+sizeof(StingerEdgeBlock)), i)
+            f(current_edge, src, etype)
+        end
+        current_eb_ptr = ebpool_priv_ptr + current_eb.next;
+    end
+end
