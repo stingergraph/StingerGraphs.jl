b98164a0bcef7a319d7d193d8a7b1f9b755783cc
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-02-09 15:56:50 -0500

    bfs using foralledges

--- src/algorithms/bfs.jl
+++ src/algorithms/bfs.jl
@@ -6,24 +6,22 @@ which is sequential and runs through every vertex in the graph. If you know
 the maximum number of active vertices, call `bfs(s, source, nv)` which is faster.
 """
 function bfs(s::Stinger, source::Int64)
-    nv = get_nv(s)
-    bfs(s, source, nv)
+    bfs(s, source, s[max_nv])
 end
 
 function bfs(s::Stinger, source::Int64, nv::Int64)
     nv>source || throw(DimensionMismatch("Attempting to run BFS with source $source in a graph with only $nv vertices."))
     parents = fill(-2, nv) #Initialize parents array with -2's.
-    parents[source+1]=-1 #Set source to -1
+    parents[source+1] = -1 #Set source to -1
     next = Vector{Int64}([source])
     sizehint!(next, nv)
     while !isempty(next)
         src = shift!(next) #Get first element
-        vertexneighbors = getsuccessors(s, src)
-        for vertex in vertexneighbors
-            #If not already set, and is not found in the queue.
-            if parents[vertex+1]==-2
-                push!(next, vertex) #Push onto queue
-                parents[vertex+1] = src
+        foralledges(s, src) do edge, src, etype
+            direction, neighbor = edgeparse(edge)
+            if (direction != 1 && parents[neighbor+1] == -2)
+                parents[neighbor+1] = src
+                push!(next, neighbor)
             end
         end
     end
