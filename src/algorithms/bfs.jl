a95b201df79d72632927529265ea5ac4fe850248
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-02-22 13:32:27 -0500

    Port bfsdistances to foralledges

--- src/algorithms/bfs.jl
+++ src/algorithms/bfs.jl
@@ -39,20 +39,26 @@ function bfskernel(s::Stinger, source::Int64, nv::Int64)
 end
 
 """
-This version is slower as it makes a call to `stinger_max_active_vertex`,
-which is sequential and runs through every vertex in the graph. If you know
-the maximum number of active vertices, call `bfsdistances(s, source, nv)` which is faster.
+This version is slower as it allocates the maximum possible vertices in the Stinger
+graph. If you know the maximum number of active vertices, call `bfsdistances(s, source, nv)`
+which is faster.
 """
 function bfsdistances(s::Stinger, source::Int64)
-    nv = get_nv(s)
-    bfsdistances(s, source, nv)
+    bfsdistances(s, source, s[max_nv])
 end
 
 """
 Obtain both the `parents` array as well as the `distances` from source.
+Empty arrays are returned on failure.
 """
 function bfsdistances(s::Stinger, source::Int64, nv::Int64)
-    nv>source || throw(DimensionMismatch("Attempting to run BFS with source $source in a graph with only $nv vertices."))
+    if source>=nv
+        return zeros(Int64, 0), zeros(Int64, 0)
+    end
+    bfsdistanceskernel(s, source, nv)
+end
+
+function bfsdistanceskernel(s::Stinger, source::Int64, nv::Int64)
     parents = fill(-2, nv) #Initialize parents array with -2's.
     distances = fill(-1, nv)
     parents[source+1]=-1 #Set source to -1
@@ -61,13 +67,13 @@ function bfsdistances(s::Stinger, source::Int64, nv::Int64)
     sizehint!(next, nv)
     while !isempty(next)
         src = shift!(next) #Get first element
-        vertexneighbors = getsuccessors(s, src)
-        for vertex in vertexneighbors
-            #If not already set, and is not found in the queue.
-            if parents[vertex+1]==-2
-                push!(next, vertex) #Push onto queue
-                parents[vertex+1] = src
-                distances[vertex+1] = distances[src+1] + 1 #Get the distance
+        foralledges(s, src) do edge, src, etype
+            direction, neighbor = edgeparse(edge)
+            #If edge direction is out and the parent is not set yet.
+            if (direction != 1 && parents[neighbor+1] == -2)
+                parents[neighbor+1] = src
+                distances[neighbor+1] = distances[src+1] + 1
+                push!(next, neighbor)
             end
         end
     end
