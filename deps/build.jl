47937b6a94f0d46e23165fd6b03134471ab8b939
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-09-06 16:51:13 -0400

    Bindeps can build stinger.

--- deps/build.jl
+++ deps/build.jl
@@ -2,22 +2,22 @@ using BinDeps
 
 @BinDeps.setup
 
-libstinger = library_dependency("libstinger")
+libstinger_core = library_dependency("libstinger_core")
 
 provides(Sources,
     URI("https://github.com/rohitvarkey/stinger/archive/stingergraphs.jl_v0.0.1.tar.gz"),
-    libstinger)
+    libstinger_core, unpacked_dir = "stinger-stingergraphs.jl_v0.0.1")
 
-stingerbuilddir = joinpath(BinDeps.depsdir(libstinger), "build")
+stingerbuilddir = joinpath(BinDeps.depsdir(libstinger_core), "src", "stinger-stingergraphs.jl_v0.0.1", "build")
 provides(BuildProcess,
     (@build_steps begin
-        GetSources(libstinger)
+        GetSources(libstinger_core)
         CreateDirectory(stingerbuilddir)
         @build_steps begin
             ChangeDirectory(stingerbuilddir)
             `cmake ..`
             `make`
         end
-    end), libstinger)
+    end), libstinger_core)
 
-@BinDeps.install Dict(:libstinger => :libstinger)
+@BinDeps.install Dict(:libstinger_core => :libstinger_core)
