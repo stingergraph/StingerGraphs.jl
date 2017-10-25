c4c20a306801deb51950cd3192aba4d716a949a8
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-10-25 18:00:53 -0400

    Add a function to update edge weights of a certain vertex

--- src/traversal.jl
+++ src/traversal.jl
@@ -1,4 +1,4 @@
-export foralledges
+export foralledges, foralledgesupdatewts
 
 """
     foralledges(f::Function, s::Stinger, v::Int64)
@@ -15,6 +15,32 @@ function foralledges(f::Function, s::Stinger, v::Int64)
         for i=1:current_eb.high
             current_edge = unsafe_load(convert(Ptr{StingerEdge}, current_eb_ptr+sizeof(StingerEdgeBlock)), i)
             f(current_edge, current_eb.vertexid, current_eb.etype)
+        end
+        current_eb_ptr = ebpool_priv_ptr + current_eb.next * (sizeof(StingerEdgeBlock) + sizeof(StingerEdge)*NUMEDGEBLOCKS);
+    end
+end
+
+"""
+    foralledgesupdatewts(s::Stinger, v::Int64, wts::Dict{Int64, Int64})
+
+Iterates over all the edges edges of a vertex and increments the weights of the
+edges if the neighbor is found in the `wts` dictionary, else leaves the weight
+unchanged.
+"""
+
+function foralledgesupdatewts(s::Stinger, v::Int64, wts::Dict{Int64, Int64})
+    ebpool_priv_ptr = storageptr(s) + s[ebpool_start] * (sizeof(UInt8)) + sizeof(UInt64) * 2
+    current_eb_ptr = ebpool_priv_ptr + getvertex(s, v).edges * (sizeof(StingerEdgeBlock) + sizeof(StingerEdge)*NUMEDGEBLOCKS)
+    while current_eb_ptr != ebpool_priv_ptr
+        current_eb = unsafe_load(convert(Ptr{StingerEdgeBlock}, current_eb_ptr))
+        for i=1:current_eb.high
+            current_edge = unsafe_load(convert(Ptr{StingerEdge}, current_eb_ptr+sizeof(StingerEdgeBlock)), i)
+            direction, neighbor = edgeparse(current_edge)
+            edge_ptr = current_eb_ptr+sizeof(StingerEdgeBlock) + (i-1) * sizeof(StingerEdge)
+            unsafe_store!(
+                convert(Ptr{Int64}, edge_ptr + sizeof(Int64)),
+                current_edge.weight + get(wts, neighbor, 0)
+            )
         end
         current_eb_ptr = ebpool_priv_ptr + current_eb.next * (sizeof(StingerEdgeBlock) + sizeof(StingerEdge)*NUMEDGEBLOCKS);
     end
